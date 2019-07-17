09bd7e4ea00b531f737d7b0c8641cfdbc0524a56
d1547156325 <1547156325@qq.com>
2019-07-17 15:06:18 +0800

    Merge branch 'master' into appMak
    App1.0.2  no OrderForm

--- src/main/java/com/noname/demo/controller/AppMerchantController.java
+++ src/main/java/com/noname/demo/controller/AppMerchantController.java
@@ -2,6 +2,9 @@ package com.noname.demo.controller;
 
 
 import com.noname.demo.entity.Merchants;
+import com.noname.demo.service.MerchantService;
+import com.noname.demo.serviceimpl.MerchantServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,8 @@ import java.util.*;
 @RequestMapping("/AppMer")
 public class AppMerchantController {
 
+    @Autowired
+    private MerchantService merchantService;
     /**
      * 查询所有商家
      * @return
@@ -19,9 +24,9 @@ public class AppMerchantController {
     @RequestMapping(value = "/allMer", method = RequestMethod.GET)
     private Map<String, Object> listArea(){
         Map<String, Object> map = new HashMap<>();
-        List<Merchants> list = null;
+        List<Merchants> list = merchantService.findAllMerchant();
         map.put("listMer", list);
-
+        System.out.println(list.get(0));
         return map;
     }
 
