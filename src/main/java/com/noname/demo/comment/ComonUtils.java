fb74cd69703c244fb55390b727cc1eb73941393a
d1547156325 <1547156325@qq.com>
2019-07-24 08:43:31 +0800

    1.8.1  小程序登录等基本功能完成

--- src/main/java/com/noname/demo/comment/ComonUtils.java
+++ src/main/java/com/noname/demo/comment/ComonUtils.java
@@ -10,9 +10,9 @@ public class ComonUtils {
 
         StringBuffer info = new StringBuffer("https://api.weixin.qq.com/sns/jscode2session?");
 
-        info.append("appid=").append("wx8d87127163bbef11").append("&");
+        info.append("appid=").append("wx75e5ecadcfeada7d").append("&");
 
-        info.append("secret=").append("94bc24f437b0e74d3a4d67d4dd577cb9").append("&");
+        info.append("secret=").append("2ffd049f802a990c0b2d098a3d748508").append("&");
 
         info.append("js_code=").append(code).append("&");
 
--- src/main/java/com/noname/demo/controller/AppLoginController.java
+++ src/main/java/com/noname/demo/controller/AppLoginController.java
@@ -1,20 +1,12 @@
 package com.noname.demo.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.noname.demo.comment.ComonUtils;
-import com.noname.demo.entity.Customers;
+import com.noname.demo.comment.RedisOperator;
+import com.noname.demo.entity.AppUserModel;
 import com.noname.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,23 +16,72 @@ public class AppLoginController {
 
     @Autowired
     CustomerService customerService = null;
+    @Autowired
+    RedisOperator redis = null;
 
+
+    /**
+     * 获取用户openid, 并将session存入redis，发送到前端
+     * @param code
+     * @return
+     */
     @RequestMapping(value = "/doLogin", method = RequestMethod.GET)
     public Map<String, Object> appDoLogin(String code){
         Map<String, Object> map = new HashMap<>();
-        System.out.println(code);
-        System.out.println(new ComonUtils().appendUrl(code));
+        if(code == null){
+            map.put("success", false);
+            return map;
+        }
+        //System.out.println(code);
+        //System.out.println(new ComonUtils().appendUrl(code));
         String url = new ComonUtils().appendUrl(code).toString();
         RestTemplate restTemplate = new RestTemplate();
         String reponse = restTemplate.getForObject(url, String.class);
+        if(reponse == null){
+            map.put("success", false);
+            return map;
+        }
         System.out.println(reponse);
         String openid = "";
-        for(int i = 0; i<reponse.length()-8; i++){
-            if(reponse.substring(i,i+6).equals("openid")){
+        String session_key = "";
+        String[] strings = reponse.split(",");
+        openid = strings[1].substring(10, strings[1].length()-2);
+        session_key = strings[0].substring(16, strings[0].length()-1);
+        AppUserModel appUserModel = new AppUserModel();
+        appUserModel.setOpenid(openid);
+        appUserModel.setSession_key(session_key);
+        redis.set("userSession:" + appUserModel.getOpenid(), appUserModel.getSession_key(),
+                60*10);
+        //System.out.println(openid);
+        map.put("sessionId","userSession:" + appUserModel.getOpenid());
+        map.put("success", true);
 
-            }
+        return map;
+    }
+
+    /**
+     * 检测session是否过期
+     */
+    @RequestMapping(value = "/session", method = RequestMethod.GET)
+    public Map<String, Object> doSession(String sessionId){
+        String resout = redis.get(sessionId);
+        Map<String, Object> map = new HashMap<>();
+        if(resout == null){
+            map.put("success", false);
+            return map;
         }
-        map.put("openid", reponse);
+        map.put("success", true);
+        return map;
+    }
+
+    /**
+     * 将sessin从redis中删除，实现退出账号
+     */
+    @RequestMapping(value = "/rvmSession",method = RequestMethod.GET)
+    public Map<String, Object> removeSession(String sessionId){
+        Map<String, Object> map = new HashMap<>();
+        redis.del(sessionId);
+        map.put("success", true);
         return map;
     }
 }
